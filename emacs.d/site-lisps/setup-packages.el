d6a864760b1fe6786afef9a4e36bc5d12ee22247
mackong <mackonghp@gmail.com>
2023-01-04 10:50:06 +0800

    emacs: add evil-org

--- emacs.d/site-lisps/setup-packages.el
+++ emacs.d/site-lisps/setup-packages.el
@@ -399,9 +399,9 @@
     (add-hook hook #'tree-sitter-hl-mode)))
 
 ;; evil
-(setq evil-want-keybinding nil)
-
 (use-package evil
+  :init
+  (setq evil-want-keybinding nil)
   :config
   (evil-mode 1)
   (evil-ex-define-cmd "q" 'kill-this-buffer)
@@ -413,6 +413,16 @@
   :ensure t
   :config
   (evil-collection-init))
+
+(use-package evil-org
+  :after org
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+            (lambda ()
+              (evil-org-set-key-theme)))
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
 
 (provide 'setup-packages)
 
