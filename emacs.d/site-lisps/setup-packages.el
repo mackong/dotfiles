7767c3352c33d73ef8b5d1643dbdb560fdd767cb
mackong <mackonghp@gmail.com>
2023-05-12 15:13:34 +0800

    emacs: fix go-translate color

--- emacs.d/site-lisps/setup-packages.el
+++ emacs.d/site-lisps/setup-packages.el
@@ -151,7 +151,7 @@
         gts-default-translator (gts-translator
                                 :picker (gts-prompt-picker)
                                 :engines (list (gts-google-engine))
-                                :render (gts-posframe-pop-render :forecolor "white" :backcolor "#222222"))))
+                                :render (gts-posframe-pop-render :forecolor "black" :backcolor "#f2eff3"))))
 
 ;; avy
 (use-package avy
