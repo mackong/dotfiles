4a32dba7806e8789bcced009f40d575a093d99fb
mackong <mackonghp@gmail.com>
2023-08-11 14:28:44 +0800

    emacs: change go-translate backcolor

--- emacs.d/site-lisps/setup-packages.el
+++ emacs.d/site-lisps/setup-packages.el
@@ -151,7 +151,7 @@
         gts-default-translator (gts-translator
                                 :picker (gts-prompt-picker)
                                 :engines (list (gts-google-engine))
-                                :render (gts-posframe-pop-render :forecolor "white" :backcolor "#24283b"))))
+                                :render (gts-posframe-pop-render :forecolor "white" :backcolor "#222222"))))
 
 ;; avy
 (use-package avy
