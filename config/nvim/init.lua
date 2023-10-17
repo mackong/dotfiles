4159e5d4972e1f0d5ca5244aa0989a50d7a38848
mackong <mackonghp@gmail.com>
2023-10-17 10:39:12 +0800

    vim: clean nmap

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -164,16 +164,11 @@ local on_attach = function(_, bufnr)
     end
 
     nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-
     nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+    nmap('gD', vim.lsp.buf.type_definition, 'Type [D]efinition')
     nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
     nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-    nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
     nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 end
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
